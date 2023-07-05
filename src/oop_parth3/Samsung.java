5eaa23016690ae64b5b1264b1a26c1904986a6ea
Arthur <at2006tur@gmail.com>
2023-07-05 22:34:34 +0200

    16-th Commit

---
+++ src/oop_parth3/Samsung.java
@@ -0,0 +1,26 @@
+package oop_parth3;
+
+public class Samsung extends TelephoneParent{
+
+
+    public Samsung(String model) {
+        super(model);
+    }
+
+    public Samsung(String model, String series) {
+        super(model, series);
+    }
+
+    public Samsung(String model, String series, int year) {
+        super(model, series, year);
+    }
+
+    public static void main(String[] args) {
+        Samsung samsung1 = new Samsung("Samsung", "S22");
+        Samsung samsung2 = new Samsung("Samsung", "S22", 2022);
+        Samsung samsung3 = new Samsung("Samsung");
+        System.out.println("Just output to the console: " + samsung1);
+        System.out.println("equals: " + samsung2.equals(samsung1));
+        System.out.println("hashCode: " + samsung3.hashCode());
+    }
+}
