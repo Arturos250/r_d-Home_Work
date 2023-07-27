559eccc1704675d71358a0f2de2d38bdd69eadb7
Arthur <at2006tur@gmail.com>
2023-07-27 11:24:05 +0200

    21-th Commit

--- src/collections/Student.java
+++ src/collections/Student.java
@@ -1,5 +1,7 @@
 package collections;
 
+import java.util.Objects;
+
 public class Student {
     private int age;
     private String name;
@@ -42,5 +44,18 @@ public class Student {
                 ", name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(name, student.name) && Objects.equals(surname, student.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, name, surname);
     }
 }
